2582a33a28f1b0ea7fab8477df839f56f9cec48a
Provash <provash20cb@gmail.com>
2026-05-14 14:12:31 +0600

    7-3 Setting Up Postgres with Neon Serverless Cloud

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg"
+
 const app :Application = express();
 const port = 5000;
 
 //middleware need / terminal undefined
-app.use(express.json());
+app.use(express.json());  //middleware
+app.use(express.text());
+// app.use(express.urlencoded());
+app.use(express.urlencoded({extended : true}));
+
+
+const pool = new Pool({
+  connectionString : "postgresql://neondb_owner:npg_SYnBF1sr9ibM@ep-nameless-sound-ap9q634f-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
 
 app.get('/', (req : Request, res : Response) => {
   // res.send('Express Server !')
@@ -14,7 +25,19 @@ app.get('/', (req : Request, res : Response) => {
 });
 
 app.post('/', async(req :Request,res : Response)=>{
-  console.log(req.body);
+  // console.log(req.body);
+  // const body = req.body;
+  // res.status(201).json({
+  //   message: "Created",
+  //   data: body,
+  // })
+  const {name,email,password} = req.body;
+  res.status(201).json({
+    message: "Created",
+    data: {
+      name, email,
+    },
+  })
 })
 
 app.listen(port, () => {
