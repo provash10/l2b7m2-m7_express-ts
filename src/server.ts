323bea0fa60812eda5f833f669efd20f4ed669d0
Provash <provash20cb@gmail.com>
2026-05-14 20:33:23 +0600

    7-5 Executing Pool and creating tables NeonDB and postgre Users

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,31 @@ app.use(express.urlencoded({extended : true}));
 
 const pool = new Pool({
   connectionString : "postgresql://neondb_owner:npg_SYnBF1sr9ibM@ep-nameless-sound-ap9q634f-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-})
+});
 
+const initDB =async()=>{
+  try{
+    await pool.query(`
+
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `)
+      console.log("Database connected successfully")
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+initDB();
 
 app.get('/', (req : Request, res : Response) => {
   // res.send('Express Server !')
